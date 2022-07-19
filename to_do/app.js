d0d6b535c9885d9cd1c92728a0785010786800d9
unknown <huzaifa.bajwa11@gmail.com>
2022-07-20 02:10:14 +0500

    Used React with to do list

--- to_do/app.js
+++ to_do/app.js
@@ -1,15 +1,55 @@
-const form = document.forms["my-form"];
-const list = document.getElementById("list")
-const input = form["task"]
+const MyComponent = () => {
+	const [arr, setArr] = React.useState([]);
+	const [inputVal, setInputVal] = React.useState("");
 
-form.addEventListener("submit", function(e){
+	const combinedList = []
 
-	e.preventDefault()	
+	for(let i=0; i<arr.length;i++){
+		combinedList.push(<li>{arr[i]}<i onClick={crossOut} className="fa-solid fa-xmark"></i></li>);
+	}
+	
+	const addTask = () =>{
+	
+		const newArr = [...arr, inputVal];
+		setArr(newArr);
 
-	list.innerHTML += `<li>${input.value}<i onclick=crossOut(this) class="fa-solid fa-xmark"></i></li>`
+	};
 
-})
+	const inputUpdater = (e) =>{
+
+		setInputVal(e.target.value);
+
+	};
+	
+	return(
+		<div> 
+		
+			<div id="background-cover">
+
+			<p className="header">Bucket List <i className="fa-solid fa-bucket"></i></p>
+
+			<input id="task-input" type="text" className="header" value={inputVal} onChange={inputUpdater} placeholder="Enter Text"></input>
+			<button type="submit" className="header" onClick={addTask}><i className="fa-solid fa-plus"></i></button> 
+			
+			</div>
+
+			<div id="tasks">
+
+				<ul id="list">
+
+					{combinedList}
+
+				</ul>
+
+			</div>
+
+		</div>
+	);
+};
 
 const crossOut = (e) => {
-	e.parentElement.classList.toggle("crossed")
+	e.target.parentElement.classList.toggle("crossed")
 }
+
+const root = ReactDOM.createRoot(document.getElementById("background-cover"));
+root.render(<MyComponent />)
